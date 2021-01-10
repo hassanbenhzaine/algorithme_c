1dfe2b305f6b4d425963b5f3da4e9992cd6244ec
Hassan Benhzaine <cbenhzaine@gmail.com>
2021-01-10 21:25:10 +0100

    change in Step2_4

--- Workshops_c/Exercices/Step2_4.c
+++ Workshops_c/Exercices/Step2_4.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
+#include<math.h>
+
+float Delta, a, b, c;
 
 main(){
 
-    int Delta, a, b, c;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    scanf("%d",&c);
-    Delta = pow(b,b)*4*a*c;
-    printf("%d",Delta);
+    // Réception des coefficients a, b et c par l'utilisateur
+    printf("Veuillez entrer les coefficients d'une equation du deuxieme degres (ax² + bx + c=0) a resoudre!\n a : ");
+    scanf("%f",&a);
+    printf(" b : ");
+    scanf("%f",&b);
+    printf(" c : ");
+    scanf("%f",&c);
+    
+    // Calcule de Delta
+    Delta = pow(b,2)-(4*a*c);
 
-
+    // Sortie des resultats
+    printf(" Delta = %.2f",Delta);
 
 }
