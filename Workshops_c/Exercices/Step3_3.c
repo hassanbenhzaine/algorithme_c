82c841cb4662fd4eb661d261900747b1764da175
hassan <cbenhzaine@gmail.com>
2021-01-12 12:30:02 +0100

    school

--- Workshops_c/Exercices/Step3_3.c
+++ Workshops_c/Exercices/Step3_3.c
@@ -1,16 +1,27 @@
-include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 main(){
+	printf("",PrintPyramyde(5));
+}
 
+//int PrintPyramyde(int a){
+
+//void PrintStar(int n){
+	for(int i = 1; i <= a;  i++){
+		printf("%d", i * 2 + 1);
+	}
+//	}
+
+//	void PrintSpace(int n){
+//		for(i = 2; ; ){
+//			printf()
+//		}
+//	}
+
+	return 1;
 }
 
 
-void PrintStar(int n){
-
-}
-
-void PrintSpace(int n){
-
-}
 
 
