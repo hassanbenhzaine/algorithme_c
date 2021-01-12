af58b7318e1537e02256479d842e9322edd958e3
Hassan Benhzaine <cbenhzaine@gmail.com>
2021-01-13 00:37:07 +0100

    Challenge2 half done

--- Workshops_c/Exercices/Challenge_final.c
+++ Workshops_c/Exercices/Challenge_final.c
@@ -1,17 +1,67 @@
 #include<stdio.h>
+#include<stdbool.h>
 
-main(){
+int num1,num2,num3,premier,num4;
 
-    printf("%d",echanger(10,20));
+/*
+bool isPremier2(int num){
+    divededby(,);
+    return ;
+} */
+
+// Fonction qui retoune la division des deux valeurs
+divededby(int n,int a){
+    return n / a;
 }
 
+// Fonction pour l'addition des deux nombres a et b
 add(int a , int b){
     return a + b;
 }
 
+// Fonction pour echanger les valeurs des deux nombres a et b
 echanger(int a, int b){
     int c = a;
     a = b;
     b = c;
-    return a,b;
+    printf("\n Le nouveaux etat des variables est\n A = %d\n B = %d",a,b);
 }
+
+// Fonction pour vérifier si un nombre il est premier ou non
+bool isPremier(int num){
+
+    // boucle "for" pour savoir si le nombre est premier ou non
+    for(int i = 2; i < num; i++){
+        if(num % i == 0){
+            premier = true;
+        }
+    }
+    return premier;
+}
+
+main(){
+
+    printf(" A = ");
+    scanf("%d",&num1);
+    printf(" B = ");
+    scanf("%d",&num2);
+
+    printf("\n L'addition de A + B = %d\n\n",add(num1,num2));
+    printf(" L'etat initial des variables sont A = %d et B = %d\n",num1,num2);
+    printf("",echanger(num1,num2));
+
+    printf("\n\n Entrer un nombre a verifier si il est premier ou non!\n ");
+    scanf("%d",&num3);
+    if(isPremier(num3) == true){
+        printf("\n %d n'est pas un nombre premier",num3);
+    } else{
+        printf("\n %d est un nombre premier",num3);
+    }
+
+}
+
+
+    
+
+
+
--- Workshops_c/Exercices/Step3_3.c
+++ Workshops_c/Exercices/Step3_3.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int a;
+
 main(){
 	printf("",PrintPyramyde(5));
 }
@@ -8,7 +10,7 @@ main(){
 //int PrintPyramyde(int a){
 
 //void PrintStar(int n){
-	for(int i = 1; i <= a;  i++){
+	for (int i = 1; i <= a;  i++){
 		printf("%d", i * 2 + 1);
 	}
 //	}
