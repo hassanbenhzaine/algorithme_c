fbd94646b1b45fdc9b799f1674884401caf302cb
hassan <cbenhzaine@gmail.com>
2021-01-13 15:02:18 +0100

    Step2_4 refiened!

--- Workshops_c/Exercices/Step2_4.c
+++ Workshops_c/Exercices/Step2_4.c
@@ -3,12 +3,15 @@
 int dividende,diviseur = 1,reste = 1,i = 1;
 
 main(){
-
+	int resultA,resultB;
+	
     // Réception des deux nombers a calculer leur pgcd
     printf("Veuillez entrer deux nombres a calculer leur pgcd!\n A : ");
     scanf("%d",&dividende);
+	resultA = dividende;
     printf(" B : ");
     scanf("%d",&diviseur);
+    resultB = diviseur;
     printf("\npgcd(%d,%d) =>\n",dividende,diviseur);
 
     // Boucle "while" pour calcule pgcd jusqu'à avoir un diviseur de 0 et un reste de 0
@@ -18,5 +21,7 @@ main(){
         dividende = diviseur;
         diviseur = reste;
     }
+    
+    printf("\nLe pgcd des nombres %d et %d : %d",resultA,resultB,dividende);
 
 }
