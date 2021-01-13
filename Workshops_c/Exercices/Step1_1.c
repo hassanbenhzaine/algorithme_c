1bbb91e7b7170348d3c069a0672d8d65db0bd406
hassan <cbenhzaine@gmail.com>
2021-01-13 17:54:03 +0100

    change

--- Workshops_c/Exercices/Step1_1.c
+++ Workshops_c/Exercices/Step1_1.c
@@ -1,27 +1,23 @@
 #include<stdio.h>
 
 main(){
-	char nom[20];
-	char prenom[20];
-	char sex[20];
-	int age;
-	long numTel;
+	char nom[20], prenom[20], sex[20], age[20], numTel[20];
 
 	
-	printf("veuiller enter vos iformations perosnnel\n");
+	printf("veuiller enter vos iformations personnel\n");
 	
 	printf("Nom : ");
 	scanf(" %s",&nom);
 	printf("Prenom : ");
 	scanf(" %s",&prenom);
 	printf("Age : ");
-	scanf(" %d",&age);
+	scanf(" %s",&age);
 	printf("Sex: ");
 	scanf(" %s",&sex);
 	printf("Numero telephone : ");
-	scanf(" %d",&numTel);
+	scanf(" %s",&numTel);
 	
-	printf("%s %s, %d ans, je suis %s et mon numero telephone est %d",nom,prenom,age,sex,numTel);
+	printf("%s %s, %s ans, je suis %s et mon numero telephone est %s\n",nom,prenom,age,sex,numTel);
 	system("pause");
 }
 
