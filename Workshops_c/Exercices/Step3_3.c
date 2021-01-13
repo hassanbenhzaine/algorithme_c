2b60bd7a26a1bb1a7624eb4852e26d6320a96085
hassan <cbenhzaine@gmail.com>
2021-01-13 14:51:18 +0100

    Step3_3 done!

--- Workshops_c/Exercices/Step3_3.c
+++ Workshops_c/Exercices/Step3_3.c
@@ -2,8 +2,10 @@
 
 
 int PrintPyramyde(int a){
+	
+	// Fonction pour affichier les espaces
 	void PrintSpace(int n){
-		char space = 's';
+		char space = ' ';
 
 		for(int i = 0; i < n; i++){
 
@@ -13,13 +15,21 @@ int PrintPyramyde(int a){
 				result--;
 			} 
 
+			// Fonction pour affichier les étoiles dans la meme ligne avant de passer a la ligne suivant
          	void PrintStar(int n){
 
 				char star = '*';
+				
 				int result2 = (i * 2)+1;
 				while(result2 != n){
 					printf("%c",star);
 					result2--;
+				} 
+				
+				int result3 = (i * 2)+1;
+				while(result3 != n+1){
+					printf("%c",star);
+					result3--;
 				} 
 				printf("\n");
 
